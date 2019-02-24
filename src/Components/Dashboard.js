35aa90da9adee469c6feb04264fa56eb7536f5dd
mayanmilis <mayanmilis16@gmail.com>
2019-02-24 14:58:09 +0200

    interface

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -5,14 +5,61 @@ import './Dashboard.css'
 
 class Invitation extends Component{ 
 state = {   
-
+    name: '',
+    number: '',
+    isComing: 0
 }
 
+onChangeHandler = (event) =>{    
+    this.setState({ 
+        [event.target.id]: event.target.value
+    })
+}
+
+addGuestHandler = () =>{    
+    let isComing = this.state.isComing
+    if(isComing<10){   
+        this.setState({ 
+            isComing: isComing+1
+        })
+    }
+}
+
+removeGuestHandler = () =>{    
+    let isComing = this.state.isComing
+    if(isComing>0){   
+        this.setState({ 
+            isComing: isComing-1
+        })
+    }else{  
+        return null
+    }
+}
+
+comingHandler = () =>{  
+    let coming = true;
+    this.setState({ 
+        isComing: coming
+    })
+}
+
+notComingHandler = () =>{  
+    let coming = false;
+    this.setState({ 
+        isComing: coming
+    })
+}
+
+
+// let name = this.state.name;
+// let number = this.state.number;
+// let isComing = this.state.isComing;
 render(){   
+    console.log(this.state.name)
     return( 
         <div className='Container'>
             <div className='Video-Wrapper'>   
-                <video src={Video} autoPlay='true' loop='true'></video>
+                <video src={Video} autoPlay={true} loop={true}></video>
             </div>
             <div className='Fade'></div>
             <div className='Overlay'>   
@@ -22,21 +69,30 @@ render(){
             </div>
             <div className='Form'>  
                 <form>  
-                    <div  className='Input'>  
-                            <h5>:נא למלא את הפרטים הבאים</h5> 
-                            <input type='text' id='name' placeholder=':שם מלא' required/>
-                            <input type='number' id='number' placeholder=':מספר טלפון' required/>
+                    <div className='DetailsContainer'>   
+                        <div  className='Details'>  
+                                <h5>:נא למלא את הפרטים הבאים</h5> 
+                                <div className='Input'>   
+                                <input type='text' id='name' placeholder=':שם מלא' onChange={this.onChangeHandler} value={this.state.name} required/>
+                                </div>
+                                <div className='Amount'>   
+                                <h5>:כמות אורחים</h5>
+                                <div className='Counter'>   
+                                <button onClick={this.removeGuestHandler}>-</button>
+                                <div className='CounterResult'>{this.state.isComing}</div>
+                                <button id='Plus' onClick={this.addGuestHandler}>+</button>
+                                </div>
+                        </div>
+                        </div>
                     </div>
-                    <div className='Amount'>   
-                            <h5>:כמות אורחים</h5>
-                            <div className='Counter'>   
-                            <button id='Minus'>-</button>
-                            <div className='CounterResult'>6</div>
-                            <button id='Plus'>+</button>
-                            </div>
-                    </div>
-                    <div className='Submit'>   
-                            <button>אישור הגעה</button>
+                    <div className='SubmitContainer'>   
+                        <div className='Submit'>   
+                                <h5>?מגיעים לאירוע</h5>
+                                <div className='Buttons'>   
+                                <button id='No'>לא מגיעים</button>
+                                <button id='Yes'>אישור הגעה</button>
+                                </div>
+                        </div>
                     </div>
                 </form>
             </div>
